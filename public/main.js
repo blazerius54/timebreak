8622eabf986a57b70c2c5c152b002394b014906e
Maxim Stepanov1 <Maxim_Stepanov1@epam.com>
2019-06-21 12:32:16 +0300

    develop/settings, linter, etc/

--- public/main.js
+++ public/main.js
@@ -1,12 +1,12 @@
-const path = require('path')
-const { app, BrowserWindow } = require('electron')
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  // Create the browser window.     
-  let win = new BrowserWindow({ width: 800, height: 600 })
+  // Create the browser window.
+  const win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.     win.loadFile('index.html')   }
   // win.loadURL(`file://${path.join(__dirname, './index.html')}`)
-  win.loadURL('http://localhost:3000/')
+  win.loadURL('http://localhost:3000/');
 }
-app.on('ready', createWindow)
+app.on('ready', createWindow);
